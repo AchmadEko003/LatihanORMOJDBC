0d23f67381dbd4173dd5f258e8b199f0c6667c28
Nine <Nine@Nine-PC>
2018-11-28 21:04:44 +0700

    Update queries

--- src/latihanormojdbc/LatihanORMOJDBC.java
+++ src/latihanormojdbc/LatihanORMOJDBC.java
@@ -7,6 +7,7 @@ package latihanormojdbc;
 
 import controllers.RegionControllers;
 import daos.EmployeeDAO;
+import daos.LocationDAO;
 import daos.RegionDAO;
 import entities.Country;
 import entities.Department;
@@ -28,10 +29,25 @@ public class LatihanORMOJDBC {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+//        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//        RegionDAO rdao = new RegionDAO(sessionFactory);
+//        RegionControllers rc = new RegionControllers(sessionFactory);
+//        EmployeeDAO edao = new EmployeeDAO(sessionFactory);
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        RegionDAO rdao = new RegionDAO(sessionFactory);
-        RegionControllers rc = new RegionControllers(sessionFactory);
-        EmployeeDAO edao = new EmployeeDAO(sessionFactory);
+        LocationDAO ldao = new LocationDAO(sessionFactory);
+        for (Location location : ldao.getAllLocation()){
+            System.out.println("Country ID" + location.getCountryId());
+            System.out.println("City" + location.getCity());
+            System.out.println("Department" + location.getDepartmentList());
+            System.out.println("Loc ID" + location.getLocationId());
+            System.out.println("Postal" + location.getPostalCode());
+            System.out.println("Province" + location.getStateProvince());
+            System.out.println("Address" + location.getStreetAddress());
+        }
+  
+//        RegionDAO rdao = new RegionDAO(sessionFactory);
+//        RegionControllers rc = new RegionControllers(sessionFactory);
+//        EmployeeDAO edao = new EmployeeDAO(sessionFactory);
 //        for (Region allregion : rdao.getAllRegion()) {
 //            System.out.println(allregion.getRegionName());
 //            for (Country country : allregion.getCountryList()) {
@@ -49,9 +65,9 @@ public class LatihanORMOJDBC {
 //            System.out.println("");
 //        }
 
-        for(Job emp : edao.selectJobId()){
-            System.out.println(emp.getJobId());
-        }
+//        for(Job emp : edao.selectJobId()){
+//            System.out.println(emp.getJobId());
+//        }
         //System.out.println(edao.getEmployeeId(98).getLastName());
 
 //        BigDecimal nil = BigDecimal.valueOf(12);
