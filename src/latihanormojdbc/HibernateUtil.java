79c9f8a6fefdf2c2813a73bcd524e29fa6f1741d
AchmadEko003 <achmadeko003@gmail.com>
2018-11-29 08:37:42 +0700

    Revert "make dao, controller, and view of department"

--- src/latihanormojdbc/HibernateUtil.java
+++ src/latihanormojdbc/HibernateUtil.java
@@ -7,9 +7,6 @@ package latihanormojdbc;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -25,11 +22,7 @@ public class HibernateUtil {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistryBuilder registry = new ServiceRegistryBuilder();
-            registry.applySettings(configuration.getProperties());
-            ServiceRegistry serviceRegistry = registry.build();
-            sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
